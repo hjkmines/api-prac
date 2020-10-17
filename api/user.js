ece87b68118fe934815262b08fb7e7a3ee9d64bc
hjkmines <hjkmines@gmail.com>
2020-10-17 13:30:13 -0600

    adds routes for user

---
+++ api/user.js
@@ -0,0 +1,14 @@
+const express = require('express'); 
+const router = express.Router; 
+
+//GET all users 
+//Public 
+router.get('/api', (req, res) => {
+    try {
+        
+    } catch (err) {
+        
+    }
+})
+
+module.exports = router 
